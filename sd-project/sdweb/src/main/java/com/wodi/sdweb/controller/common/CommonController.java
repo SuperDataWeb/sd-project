7a9cb3cefe845609ee0f4df011df36a1c02f8f8d
redbra <53850513@qq.com>
2014-09-27 19:16:39 +0800

    [*] 产品controller

--- sd-project/sdweb/src/main/java/com/wodi/sdweb/controller/common/CommonController.java
+++ sd-project/sdweb/src/main/java/com/wodi/sdweb/controller/common/CommonController.java
@@ -1,16 +1,23 @@
 package com.wodi.sdweb.controller.common;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.wodi.sdweb.model.SpProductType;
+import com.wodi.sdweb.service.SpProductTypeService;
 
 @Controller
 @RequestMapping("home")
 public class CommonController {
 	
+	@Autowired
+	private SpProductTypeService spProductTypeService;
+	
 	@RequestMapping("/frame.do")
 	public ModelAndView indexFrame(String targetFrame, String targetPage) {
-		ModelAndView model = new ModelAndView("../../index");
+		ModelAndView model = new ModelAndView("../../index1");
 		model.addObject("targetFrame", targetFrame);
 		model.addObject("targetPage", targetPage);
 		return model;
@@ -19,5 +26,18 @@ public class CommonController {
 	@RequestMapping("/homePage.do")
 	public ModelAndView homePage() {
 		return new ModelAndView("homePage");
+	}
+	
+	@RequestMapping("/homePage1.do")
+	public ModelAndView homePage1() {
+		ModelAndView model = new ModelAndView("homePage1");
+		SpProductType spProductTypeSmall =  spProductTypeService.selectByTypeId(1l);
+		SpProductType spProductTypeMiddle =  spProductTypeService.selectByTypeId(1l);
+		SpProductType spProductTypeBusiness =  spProductTypeService.selectByTypeId(1l);
+		
+		model.addObject("spProductTypeSmall", spProductTypeSmall);
+		model.addObject("spProductTypeMiddle", spProductTypeMiddle);
+		model.addObject("spProductTypeBusiness", spProductTypeBusiness);
+		return model;
 	}
 }
--- sd-project/sdweb/src/main/java/com/wodi/sdweb/controller/product/ProductController.java
+++ sd-project/sdweb/src/main/java/com/wodi/sdweb/controller/product/ProductController.java
@@ -1,18 +1,40 @@
 package com.wodi.sdweb.controller.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.wodi.sdweb.model.SpProduct;
+import com.wodi.sdweb.model.SpProductType;
+import com.wodi.sdweb.service.SpProductService;
+import com.wodi.sdweb.service.SpProductTypeService;
+
 @Controller
 //@RequestMapping("/product")
 public class ProductController {
+	
+	@Autowired
+	private SpProductTypeService spProductTypeService;
+	
+	@Autowired
+	private SpProductService spProductService;
 	
 	@RequestMapping("product_{productType}/frame.do")
 	public ModelAndView productFrame(@PathVariable String productType, String param) {
 		ModelAndView model = new ModelAndView("Product/product_frame_" + productType);
 		model.addObject("target", param);
+		return model;
+	}
+	
+	@RequestMapping("product/frame.do")
+	public ModelAndView productFrameType(String productTypeId, String productId) {
+		ModelAndView model = new ModelAndView("Product/product_frame");
+		Long typeId = Long.parseLong(productTypeId);
+		SpProductType spProductType  = spProductTypeService.selectByTypeId(typeId);
+		model.addObject("productType", spProductType);
+		model.addObject("productId", productId);
 		return model;
 	}
 	
@@ -34,5 +56,15 @@ public class ProductController {
 	@RequestMapping("product/industry/{productIndex}.do")
 	public ModelAndView industryProduct(@PathVariable String productIndex) {
 		return new ModelAndView("Product/industry/" + productIndex);
+	}
+	
+	@RequestMapping("product.do")
+	public ModelAndView industryProduct(String productTypeId, String productId) {
+		ModelAndView model = new ModelAndView("Product/product");
+		Long id = Long.parseLong(productId);
+		SpProduct product = spProductService.selectById(id);
+		model.addObject("product", product);
+		model.addObject("productId", productId);
+		return model;
 	}
 }
